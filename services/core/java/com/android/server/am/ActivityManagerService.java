7541e827fa0a31419b7b88c520e38ec03c536cc9
humor2008 <humorz@gmail.com>
2016-06-13 19:11:57 +0800

    Add ForceStop
    Change-Id: I7f56f35a43deb407a1988661400eb9d309006f68

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -2527,6 +2527,7 @@ public final class ActivityManagerService extends ActivityManagerNative
         mSystemThread = ActivityThread.currentActivityThread();
 
         Slog.i(TAG, "Memory class: " + ActivityManager.staticGetMemoryClass());
+        PreventRunningUtils.init(this);
 
         mHandlerThread = new ServiceThread(TAG,
                 android.os.Process.THREAD_PRIORITY_FOREGROUND, false /*allowIo*/);
@@ -3321,6 +3322,9 @@ public final class ActivityManagerService extends ActivityManagerNative
             boolean knownToBeDead, int intentFlags, String hostingType, ComponentName hostingName,
             boolean allowWhileBooting, boolean isolated, int isolatedUid, boolean keepIfLarge,
             String abiOverride, String entryPoint, String[] entryPointArgs, Runnable crashHandler) {
+        if (!PreventRunningUtils.hookStartProcessLocked(info, knownToBeDead, intentFlags, hostingType, hostingName)) {
+            return null;
+        }
         long startTime = SystemClock.elapsedRealtime();
         ProcessRecord app;
         if (!isolated) {
@@ -4083,9 +4087,10 @@ public final class ActivityManagerService extends ActivityManagerNative
     public final int startActivity(IApplicationThread caller, String callingPackage,
             Intent intent, String resolvedType, IBinder resultTo, String resultWho, int requestCode,
             int startFlags, ProfilerInfo profilerInfo, Bundle options) {
-        return startActivityAsUser(caller, callingPackage, intent, resolvedType, resultTo,
+        return PreventRunningUtils.onStartActivity(
+            startActivityAsUser(caller, callingPackage, intent, resolvedType, resultTo,
             resultWho, requestCode, startFlags, profilerInfo, options,
-            UserHandle.getCallingUserId());
+            UserHandle.getCallingUserId()), caller, intent);
     }
 
     @Override
@@ -4960,6 +4965,7 @@ public final class ActivityManagerService extends ActivityManagerNative
                 }
                 Slog.i(TAG, "Process " + app.processName + " (pid " + pid
                         + ") has died");
+                PreventRunningUtils.onAppDied(app);
                 mAllowLowerMemLevel = true;
             } else {
                 // Note that we always want to do oom adj to update our state with the
@@ -5848,7 +5854,7 @@ public final class ActivityManagerService extends ActivityManagerNative
                 // that match it.  We need to qualify this by the processes
                 // that are running under the specified app and user ID.
                 } else {
-                    final boolean isDep = app.pkgDeps != null
+                    final boolean isDep = PreventRunningUtils.isDep(app.pkgDeps)
                             && app.pkgDeps.contains(packageName);
                     if (!isDep && UserHandle.getAppId(app.uid) != appId) {
                         continue;
@@ -9035,6 +9041,8 @@ public final class ActivityManagerService extends ActivityManagerNative
                 pr.waitingToKill = "remove task";
             }
         }
+
+        PreventRunningUtils.onCleanUpRemovedTask(component);
     }
 
     private void removeTasksByPackageNameLocked(String packageName, int userId) {
@@ -9189,7 +9197,8 @@ public final class ActivityManagerService extends ActivityManagerNative
                         mStackSupervisor.showLockTaskToast();
                         return false;
                     }
-                    return ActivityRecord.getStackLocked(token).moveTaskToBackLocked(taskId);
+                    return PreventRunningUtils.onMoveActivityTaskToBack(
+                        ActivityRecord.getStackLocked(token).moveTaskToBackLocked(taskId), token);
                 }
             } finally {
                 Binder.restoreCallingIdentity(origId);
@@ -16147,9 +16156,11 @@ public final class ActivityManagerService extends ActivityManagerNative
         synchronized(this) {
             final int callingPid = Binder.getCallingPid();
             final int callingUid = Binder.getCallingUid();
+            PreventRunningUtils.setSenderInStartService(caller);
             final long origId = Binder.clearCallingIdentity();
-            ComponentName res = mServices.startServiceLocked(caller, service,
-                    resolvedType, callingPid, callingUid, callingPackage, userId);
+            ComponentName res = PreventRunningUtils.clearSenderInStartService(
+                mServices.startServiceLocked(caller, service,
+                    resolvedType, callingPid, callingUid, callingPackage, userId));
             Binder.restoreCallingIdentity(origId);
             return res;
         }
@@ -16380,8 +16391,10 @@ public final class ActivityManagerService extends ActivityManagerNative
         }
 
         synchronized(this) {
-            return mServices.bindServiceLocked(caller, token, service,
-                    resolvedType, connection, flags, callingPackage, userId);
+            PreventRunningUtils.setSenderInBindService(caller);
+            return PreventRunningUtils.clearSenderInBindService(
+                mServices.bindServiceLocked(caller, token, service,
+                    resolvedType, connection, flags, callingPackage, userId));
         }
     }
 
@@ -17414,12 +17427,14 @@ public final class ActivityManagerService extends ActivityManagerNative
             final ProcessRecord callerApp = getRecordForAppLocked(caller);
             final int callingPid = Binder.getCallingPid();
             final int callingUid = Binder.getCallingUid();
+            PreventRunningUtils.setSenderInBroadcastIntent(caller);
             final long origId = Binder.clearCallingIdentity();
-            int res = broadcastIntentLocked(callerApp,
+            int res = PreventRunningUtils.onBroadcastIntent(
+                broadcastIntentLocked(callerApp,
                     callerApp != null ? callerApp.info.packageName : null,
                     intent, resolvedType, resultTo, resultCode, resultData, resultExtras,
                     requiredPermissions, appOp, null, serialized, sticky,
-                    callingPid, callingUid, userId);
+                    callingPid, callingUid, userId), intent);
             Binder.restoreCallingIdentity(origId);
             return res;
         }
